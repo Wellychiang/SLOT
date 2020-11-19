bd79895a217f3f5f3cf36d3dae6f08c2d22e368c
welly.chiang <welly.chiang@nexiosoft.com>
2020-11-19 13:48:15 +0800

    'I_forgoot_execute'

--- execute.py
+++ execute.py
@@ -3,9 +3,9 @@ import pytest
 
 if __name__ == '__main__':
 
-    pytest.main(['-vvsm', 'd'])
+    # pytest.main(['-vvsm', 'd'])
     # pytest.main(['-vvs'])
 
-    # pytest.main(['-vvs', '--alluredir', 'report'])
+    pytest.main(['-vvs', '--alluredir', 'report'])
 
 
