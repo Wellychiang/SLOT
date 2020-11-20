0ce462714b6d9bea34a725a851ddace914dccde3
welly.chiang <welly.chiang@nexiosoft.com>
2020-11-20 18:53:48 +0800

    'goodgood'

--- execute.py
+++ execute.py
@@ -4,8 +4,8 @@ import pytest
 if __name__ == '__main__':
 
     # pytest.main(['-vvsm', 'd'])
-    pytest.main(['-vvs'])
+    # pytest.main(['-vvs'])
 
-    # pytest.main(['-vvs', '--alluredir', 'report'])
+    pytest.main(['-vvs', '--alluredir', 'report'])
 
 
