9ce14a208c18b54f504454bdd1e13fb900f6b216
welly.chiang <welly.chiang@nexiosoft.com>
2021-01-06 11:14:06 +0800

    'fixed_rerun'

--- execute.py
+++ execute.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     # pytest.main(['-vvsm', 'd'])
     # pytest.main(['-vvs'])
 
-    pytest.main(['-vvs', '--reruns', '--reruns-delay', '2', '--alluredir', 'report'])
+    pytest.main(['-vvs', '--reruns', '2', '--reruns-delay', '2', '--alluredir', 'report'])
