dccc1b6c4985cb67c7aa2dc44dd644942b95d8a5
welly.chiang <welly.chiang@nexiosoft.com>
2021-02-19 16:07:51 +0800

    execute.py

--- execute.py
+++ execute.py
@@ -3,7 +3,7 @@ import pytest
 
 if __name__ == '__main__':
 
-    pytest.main(['-vvsm', 'd'])
+    # pytest.main(['-vvsm', 'd'])
     # pytest.main(['-vvs'])
 
-    # pytest.main(['-vvs', '--reruns', '2', '--reruns-delay', '2', '--alluredir', 'report'])
+    pytest.main(['-vvs', '--reruns', '2', '--reruns-delay', '2', '--alluredir', 'report'])
